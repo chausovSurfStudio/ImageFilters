0671d485a16cca5e58c2e9b3ac0d547787b8f5f8
Александр Чаусов <chausov@surfstudio.ru>
2020-10-17 19:49:29 +0300

    filter controller layout

--- ImageFilters/Screens/Filters/FiltersViewController.swift
+++ ImageFilters/Screens/Filters/FiltersViewController.swift
@@ -9,14 +9,53 @@ import UIKit
 
 class FiltersViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        navigationItem.title = "Фильтры"
+    private enum Constants {
+        static let identifier = "defaultCell"
     }
 
+    @IBOutlet private weak var tableView: UITableView!
 
-    @IBAction private func applyFilter(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureAppearance()
+    }
+
+}
+
+// MARK: - UITableViewDataSource
+
+extension FiltersViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Constants.identifier) else {
+            return UITableViewCell()
+        }
+        cell.textLabel?.text = "asd"
+        return cell
+    }
+
+}
+
+// MARK: - UITableViewDelegate
+
+extension FiltersViewController: UITableViewDelegate {
+
+}
+
+// MARK: - Private Methods
+
+private extension FiltersViewController {
+
+    func configureAppearance() {
+        navigationItem.title = "Фильтры"
+        tableView.separatorInset = .init(top: 0, left: 16, bottom: 0, right: 16)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: Constants.identifier)
     }
 
 }
